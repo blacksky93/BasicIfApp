06e71f22a4cd5d676c755a0a8fc6fd13b3d63851
mgapp <mgapp@MIKEGYO>
2025-06-03 18:12:50 -0400

    Add project files.

---
+++ BasicIf/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name: ");
+            string? firstName = Console.ReadLine();
+
+            if (firstName.ToLower() == "tim")
+            {
+                Console.WriteLine("Hell Mr. Corey");
+            }
+            else
+            {
+                Console.WriteLine($"Hello {firstName}");
+            }
+            Console.WriteLine("End of Program.");
+        }
+    }
+}
